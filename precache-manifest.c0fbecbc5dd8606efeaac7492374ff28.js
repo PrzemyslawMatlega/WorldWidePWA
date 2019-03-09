c098f024d6e79e3502712703a4f91579603265c9
PrzemyslawMatlega <przemekmat@yahoo.com>
2019-03-09 17:02:59 +0100

    Updates

---
+++ precache-manifest.c0fbecbc5dd8606efeaac7492374ff28.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "055966d8fc17023d686c",
+    "url": "style.css"
+  },
+  {
+    "revision": "055966d8fc17023d686c",
+    "url": "main.js"
+  },
+  {
+    "revision": "055966d8fc17023d686c",
+    "url": "index.html"
+  }
+];
